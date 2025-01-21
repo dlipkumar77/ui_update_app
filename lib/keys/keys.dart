bd38c96208f545a5bb9839c8ec331c807702c39f
deepak <dlipkumar77@gmail.com>
2025-01-21 17:54:13 +0530

    unique keys added to solve the checked items

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -71,6 +71,8 @@ class _KeysState extends State<Keys> {
               // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
               for (final todo in _orderedTodos)
                 CheckableTodoItem(
+                  //key: ValueKey(todo.text), //Prefferable
+                  key: ObjectKey(todo),
                   todo.text,
                   todo.priority,
                 ),
